08458dedaa6807e7a6a04c56116ee1cd3e10d022
Juan Bajonero <juan.bajonero.gdcan@gmail.com>
2026-03-18 15:26:50 -0700

    Improvements

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
         "https://*.vercel.app",
         "https://frontend.vercel.app",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
